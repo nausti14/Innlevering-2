173a8554e4da0d844fe7287eae8c35f878479827
chauthaing <chau.thai.ng@gmail.com>
2016-02-18 13:18:46 +0100

    j

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 function changeImage(element) {
-    var right = '/assets/ferdigfaen-01.png';
-    var left = '/assets/rass-01.png';
+    var right = 'assets/ferdigfaen-01.png';
+    var left = 'assets/rass-01.png';
     element.src = element.bln ? right : left;
     element.bln = !element.bln;
     if (!element.bln) {
@@ -12,7 +12,7 @@ function changeImage(element) {
 var audioElement;
 $(document).ready(function () {
     audioElement = document.createElement('audio');
-    audioElement.setAttribute('src', '/assets/277466__craftport__the-sound-of-a-forest-after-rain-drops-falling-on-ferns-rear.wav');
+    audioElement.setAttribute('src', 'assets/277466__craftport__the-sound-of-a-forest-after-rain-drops-falling-on-ferns-rear.wav');
     audioElement.setAttribute('autoplay', 'autoplay');
     $.get();
     audioElement.addEventListener('load', function () {
